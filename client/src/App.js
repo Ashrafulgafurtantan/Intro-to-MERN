06627450f84aa698f96376714f6161f542eb50d0
Dexter <bsse1021@iit.du.ac.bd>
2020-07-31 05:51:58 +0600

    Added GET method

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-import React from "react";
-import axios from "axios";
+import "./App.css";
+import React, { useEffect } from "react";
+const axios = require("axios");
+
 function App() {
   const [story, setStory] = React.useState({
     title: "",
     body: "",
   });
+  const [posts, setPosts] = React.useState([]);
+  //const[loading,setLoading]=React.useState(false);
 
+  //...............  Function ...................
   function handleSubmit(event) {
-    event.preventDefault();
+    //   event.preventDefault();
     const payload = {
       title: story.title,
       body: story.body,
     };
-
     axios({
       url: "http://localhost:5000/api/save",
       method: "POST",
@@ -21,11 +25,25 @@ function App() {
       .then(() => {
         console.log("data sent success");
         setStory({ title: "", body: "" });
+        //  setLoading(true);
       })
       .catch(() => {
         console.log("data sent failure");
       });
   }
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api")
+      .then((response) => {
+        console.log(response.data);
+        setPosts(response.data);
+        //  setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   function handleChange(event) {
     const { name, value } = event.target;
 
@@ -37,9 +55,9 @@ function App() {
     });
   }
   return (
-    <div>
+    <div className=".app">
       <h1>Hello Black!!!</h1>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div className="from-input">
           <input
             //   autoComplete="off"
@@ -62,6 +80,16 @@ function App() {
         </div>
         <button onClick={handleSubmit}>Submit</button>
       </form>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <div key={post.id} className="story">
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 }
